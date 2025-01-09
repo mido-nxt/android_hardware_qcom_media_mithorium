78465040ef9032239a007507b48314bca95b1d8a
Michael Bestas <mkbestas@lineageos.org>
2025-01-09 09:19:20 +0200

    mm-video-v4l2: vidc: Drop support for hypervisor
    This isn't used by any supported platform
    
    Change-Id: I09de8028e4b0f857286fa51974431c7d93c9fdd4

--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -61,7 +61,6 @@ libmm-vdec-inc          += $(call project-path-for,qcom-media)/mm-core/inc
 libmm-vdec-inc          += $(TARGET_OUT_HEADERS)/qcom/display
 libmm-vdec-inc          += $(TARGET_OUT_HEADERS)/adreno
 libmm-vdec-inc          += $(call project-path-for,qcom-media)/libc2dcolorconvert
-libmm-vdec-inc          += $(call project-path-for,qcom-media)/hypv-intercept
 libmm-vdec-inc          += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
 # Common Dependencies
@@ -85,10 +84,6 @@ else ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_SUPPORT_MAX_H264_LEV
 libmm-vdec-def += -DMAX_H264_LEVEL_51
 endif
 
-# Hypervisor
-ifeq ($(ENABLE_HYP),true)
-libmm-vdec-def += -D_HYPERVISOR_
-endif
 # ---------------------------------------------------------------------------------
 # 			Make the Shared library (libOmxVdec)
 # ---------------------------------------------------------------------------------
@@ -114,7 +109,7 @@ LOCAL_ADDITIONAL_DEPENDENCIES   := $(libmm-vdec-add-dep)
 LOCAL_PRELINK_MODULE    := false
 LOCAL_SHARED_LIBRARIES  := liblog libcutils libdl libqdutils libion
 
-LOCAL_SHARED_LIBRARIES  += libqdMetaData libhypv_intercept
+LOCAL_SHARED_LIBRARIES  += libqdMetaData
 
 LOCAL_SRC_FILES         := src/frameparser.cpp
 LOCAL_SRC_FILES         += src/h264_utils.cpp
