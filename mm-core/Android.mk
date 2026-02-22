8351ccef2dff75cf66d6ac99b43b1fa650d5e5d8
Nalinish Ranjan <nalinishranjan05@gmail.com>
2026-02-22 18:41:05 +0530

    Revert "media: Bring back LOCAL_COPY_HEADERS optionally"
    This reverts commit 2cf7d422961cdebf386390aea8236fc5c73861a3.

--- mm-core/Android.mk
+++ mm-core/Android.mk
@@ -66,7 +66,6 @@ endif
 #             Deploy the headers that can be exposed
 #===============================================================================
 
-ifeq ($(BUILD_BROKEN_USES_BUILD_COPY_HEADERS),true)
 LOCAL_COPY_HEADERS_TO   := mm-core/omxcore
 LOCAL_COPY_HEADERS      := inc/OMX_Audio.h
 LOCAL_COPY_HEADERS      += inc/OMX_Component.h
@@ -95,7 +94,6 @@ LOCAL_COPY_HEADERS      += inc/OMX_VideoExt.h
 LOCAL_COPY_HEADERS      += inc/QOMX_StreamingExtensions.h
 LOCAL_COPY_HEADERS      += inc/QCMediaDefs.h
 LOCAL_COPY_HEADERS      += inc/QCMetaData.h
-endif
 
 #===============================================================================
 #             LIBRARY for Android apps
